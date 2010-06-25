8f0c1178fc53035160970eba6745ad31e75a272d
Doug Johnston <doug.johnston@gmail.com>
2010-06-25 11:02:53 -0700

    First rev

---
+++ OWAMonitor/MainWindow.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace OWAMonitor
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.messageTextBox.Location = new System.Drawing.Point(0, 115);
+            this.messageTextBox.Margin = new System.Windows.Forms.Padding(5);
+            this.messageTextBox.Multiline = true;
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.ReadOnly = true;
+            this.messageTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.messageTextBox.Size = new System.Drawing.Size(292, 158);
+            this.messageTextBox.TabIndex = 0;
+            this.messageTextBox.WordWrap = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "Double Click";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "OWAMonitor";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(15, 34);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(265, 20);
+            this.emailTextBox.TabIndex = 1;
+            this.emailTextBox.Text = "pat@example.com";
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(205, 60);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 4;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-3, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Messages";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "email:";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.messageTextBox);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ OWAMonitor/MainWindow.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Exchange.WebServices.Data;
+using Growl.Connector;
+
+
+namespace OWAMonitor
+{
+    public partial class MainWindow : Form
+    {
+        private readonly GrowlConnector growl;
+        private readonly NotificationType notificationType;
+        private Growl.Connector.Application application;
+        private const string SampleNotificationType = "OWAMonitor";
+        private Timer _timer;
+        private FormWindowState _mPreviousWindowState;
+        private readonly ExchangeService _exchangeService;
+        private PullSubscription _ps;
+        private DateTime _latestMessageDate = DateTimeOffset.Now.LocalDateTime;
+        private FindFoldersResults _folders;
+        /* Using this doesn't work either
+        private PropertySet _propertySet = new PropertySet(BasePropertySet.IdOnly)
+                                  {
+                                      ItemSchema.Body
+                                  };
+        */
+        public MainWindow()
+        {
+            InitializeComponent();
+            notificationType = new NotificationType(SampleNotificationType, "Sample Notification");
+            growl = new GrowlConnector();
+            growl.NotificationCallback += new GrowlConnector.CallbackEventHandler(GrowlNotificationCallback);
+            RegisterGrowl();
+
+            growl.EncryptionAlgorithm = Cryptography.SymmetricAlgorithmType.PlainText;            
+            _exchangeService = new ExchangeService(ExchangeVersion.Exchange2007_SP1);
+            //_exchangeService.Credentials = new NetworkCredential("{Active Directory ID}", "{Password}", "{Domain Name}");
+
+            bool success = ConfigureExchange();
+
+            if (!success)
+            {
+                messageTextBox.AppendText("Not connected");
+            }
+            
+        }
+
+        protected bool ConfigureExchange()
+        {           
+            try
+            {
+                _exchangeService.AutodiscoverUrl(emailTextBox.Text);
+            }
+            catch (Exception e)
+            {
+                messageTextBox.Text += e;
+                return false;
+            }
+            try
+            {
+                _folders = _exchangeService.FindFolders(WellKnownFolderName.Inbox, new FolderView(50));
+            }
+            catch (Exception e)
+            {
+                messageTextBox.Text += e;
+                return false;
+            }
+
+            _timer = new Timer { Interval = 1000 * 10 };
+            _timer.Tick += CheckMailHandler;
+            _timer.Start();
+            CheckMailHandler(null, null);
+         
+            return true;
+        }
+
+        protected void CheckMailHandler(object o, EventArgs e)
+        {
+            _timer.Stop();
+                        
+            FindItemsResults<Item> findResults;
+            SearchFilter sf = new SearchFilter.IsGreaterThan(ItemSchema.DateTimeReceived, _latestMessageDate);
+            
+            //process the Inbox            
+            findResults = _exchangeService.FindItems(WellKnownFolderName.Inbox, sf, new ItemView(10));
+            if (findResults.Items.Count > 0)
+                ProcessResults(findResults);
+
+            // and process all the other folders too
+            foreach (Folder folder in _folders.Folders)
+            {
+                findResults = _exchangeService.FindItems(folder.Id, sf, new ItemView(10));
+                if (findResults.Items.Count > 0)
+                    ProcessResults(findResults);
+            }
+            _timer.Start();
+        }
+
+        private void ProcessResults(FindItemsResults<Item> results)
+        {
+            //_exchangeService.LoadPropertiesForItems(results.Items, _propertySet); //this throws exception
+            foreach (Item item in results.Items)
+            {
+                //item.Load(); //this throws exception
+                if (item.DateTimeReceived > _latestMessageDate)
+                {
+                    _latestMessageDate = item.DateTimeReceived + TimeSpan.FromSeconds(1);
+                }
+                EmailMessage email = (EmailMessage)item;
+                if (email.IsRead) continue;
+                string indicator = "";
+                if (item.Importance == Importance.High) indicator += "!";
+                if (email.ToRecipients.Count == 0 && email.CcRecipients.Count == 0) indicator += ">>";
+                SendGrowl(indicator + " " + email.From.Name, item.Subject);                
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);          
+            if (WindowState != FormWindowState.Minimized) _mPreviousWindowState = WindowState;            
+            notifyIcon1.Visible = (WindowState == FormWindowState.Minimized);
+            Visible = !notifyIcon1.Visible;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Visible = true;
+            notifyIcon1.Visible = false;
+            WindowState = _mPreviousWindowState;
+        }
+        private void RegisterGrowl()
+        {
+            application = new Growl.Connector.Application("OWAMonitor");
+            growl.Register(application, new[] { notificationType });
+        }
+
+        private void SendGrowl(string title, string message)
+        {
+            CallbackContext callbackContext = new CallbackContext("", "");
+
+            Notification notification = new Notification(application.Name, notificationType.Name, DateTime.Now.Ticks.ToString(), title, message);
+            growl.Notify(notification, callbackContext);
+        }
+
+        static void GrowlNotificationCallback(Response response, CallbackData callbackData)
+        {
+            //string text = String.Format("Response Type: {0}\r\nNotification ID: {1}\r\nCallback Data: {2}\r\nCallback Data Type: {3}\r\n", callbackData.Result, callbackData.NotificationID, callbackData.Data, callbackData.Type);
+            //MessageBox.Show(text, "Callback received", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            ConfigureExchange();
+        }
+    }
+}
